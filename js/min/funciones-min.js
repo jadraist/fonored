4ea668bb5cb69e80762dc018ae00503911734dff
Jadra Campaña <jadra.fer@gmail.com>
2017-01-31 12:40:40 -0300

    Update
    Update

---
+++ js/min/funciones-min.js
@@ -0,0 +1,1 @@
+$(document).ready(function(){$('a[href^="#"]').on("click",function(t){var e=$(this.getAttribute("href"));e.length&&(t.preventDefault(),$("html, body").stop().animate({scrollTop:e.offset().top},1e3))})});
